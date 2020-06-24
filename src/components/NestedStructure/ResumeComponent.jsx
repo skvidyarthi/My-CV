02f2865cf99d7236c83ab25e25c76333a3325ef4
skvidyarthi <sanjit.vidyarthi@gmail.com>
2020-06-24 06:56:29 +0530

    updated

--- src/components/NestedStructure/ResumeComponent.jsx
+++ src/components/NestedStructure/ResumeComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 class ResumeComponent extends React.Component{
+    
 
     render(){
         return(
@@ -10,13 +11,12 @@ class ResumeComponent extends React.Component{
                 <div className="section-inner start-page-full-width">
                 <div className="start-page-full-width">
                         <h2 className="section-title">Resume</h2>
-                                                <h5 className="text-right">7 Years of Experience</h5>
+                                                <h5 className="text-right">12 Years of Experience</h5>
 
 <div className="col-xs-12 col-sm-8 col-md-8 col-lg-7 pull-left">
 <div className="edu">
 <h3>Education</h3>
 
-	
 </div>
 
 
@@ -24,7 +24,32 @@ class ResumeComponent extends React.Component{
 <div className="col-xs-12 col-sm-4 col-md-4 col-lg-4 pull-right">
 <div className="skill">
 <h3>Coding Skills</h3>
+<div className="skills">
+    <h4>JavaScript<span>75%</span></h4>
+    <div className="skill-border">
+    <div className="progress">
+        <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: '75%'}}></div>
+    </div>
+</div>
+<h4>HTML/CSS<span>95%</span></h4>
+<div className="skill-border">
+    <div className="progress"><div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style={{width: '95%'}}></div></div>
+</div>
+<h4>Jquery<span>85%</span></h4>
+<div className="skill-border">
+    <div className="progress"><div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" style={{width: '85%'}}></div></div>
+</div>
+<h4>Bootstrap<span>95%</span></h4>
+<div className="skill-border">
+    <div className="progress"><div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style={{width: '95%'}}></div></div>
+</div>
+<h4>React<span>25%</span></h4>
+<div className="skill-border">
+    <div className="progress"><div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '25%'}}></div></div>
+</div>
 
+
+</div>
 
 </div>
 
